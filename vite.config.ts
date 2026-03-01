c9d7ee58bb8207fd852ad07d29fdf60e9f9b7356
Shakadak <Shakadak@users.noreply.github.com>
2026-03-01 21:49:24 +0100

    feat: PWA plugin test

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import solid from 'vite-plugin-solid'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
   base: "/ranked-voting",
-  plugins: [solid()],
+  plugins: [
+    solid(),
+    VitePWA({
+      registerType: 'autoUpdate',
+    }),
+  ],
 })
