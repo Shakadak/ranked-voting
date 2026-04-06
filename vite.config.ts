a5ba12c0cec6727980fce0d782ad3b87f61a57ca
Nathanael <Shakadak@users.noreply.github.com>
2026-04-06 19:50:09 +0200

    preserve state between hot reload

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite'
 import solid from 'vite-plugin-solid'
+import solidBetterRefresh from 'solid-better-refresh'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
   base: "/ranked-voting",
   plugins: [
     solid(),
+    solidBetterRefresh(),
     VitePWA({
       registerType: 'autoUpdate',
     }),
