40f142a48dcce99a96083dcce8bb329a32939a04
Nathanael <Shakadak@users.noreply.github.com>
2026-04-06 19:51:01 +0200

    add file aliases

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import solid from 'vite-plugin-solid'
 import solidBetterRefresh from 'solid-better-refresh'
 import { VitePWA } from 'vite-plugin-pwa'
+import path from 'path'
 
 export default defineConfig({
   base: "/ranked-voting",
@@ -12,4 +13,11 @@ export default defineConfig({
       registerType: 'autoUpdate',
     }),
   ],
+  resolve: {
+    alias: {
+      "@assets": path.resolve(__dirname, "src/assets"),
+      "@components": path.resolve(__dirname, "src/components"),
+      "@lib": path.resolve(__dirname, "src/lib"),
+    },
+  },
 })
